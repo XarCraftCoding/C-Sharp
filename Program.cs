f48dd96197bfbe760bda96ffe6b0819f20032eb4
XarCraftCoding <117906852+XarCraftCoding@users.noreply.github.com>
2023-06-22 18:54:23 +0300

    Update 1.5

--- Program.cs
+++ Program.cs
@@ -6,12 +6,30 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // constants = immutable values which are known at compile time
-            //             and do not change for the life of the program
+            // type casting = Converting a value to a different data type
+            //                Useful when we accept user input (string)
+            //                Different data types can do different things
 
-            const double pi = 3.14159;
+            double a = 3.14;
+            int b = Convert.ToInt32(a);
 
-            Console.WriteLine(pi);
+            int c = 123;
+            double d = Convert.ToDouble(c) + 0.1;
+
+            int e = 321;
+            String f = Convert.ToString(e);
+
+            String g = "$";
+            char h = Convert.ToChar(g);
+
+            String i = "true";
+            bool j = Convert.ToBoolean(i);
+
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(f.GetType());
+            Console.WriteLine(h.GetType());
+            Console.WriteLine(j.GetType());
 
             Console.ReadKey();
         }
