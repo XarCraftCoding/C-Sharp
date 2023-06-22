2d5c017c2500aab65bd552ffbd9ba207c071beef
XarCraftCoding <117906852+XarCraftCoding@users.noreply.github.com>
2023-06-22 18:43:55 +0300

    Update 1.4

--- Program.cs
+++ Program.cs
@@ -6,29 +6,12 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int x; // declaration
-            x = 123; // initialization
+            // constants = immutable values which are known at compile time
+            //             and do not change for the life of the program
 
-            int y = 321; // declaration + initialization
+            const double pi = 3.14159;
 
-            int z = x + y;
-
-            int age = 21; // whole integer
-            double height = 300.5; // decimal number
-            bool alive = false; //true or false
-            char symbol = '@';
-            String name = "Bro";
-
-            Console.WriteLine("Hello " + name);
-            Console.WriteLine(x);
-            Console.WriteLine("Your age is " + age);
-            Console.WriteLine("Your height is " + height + "cm");
-            Console.WriteLine("Are you alive? " + alive);
-            Console.WriteLine("YOur symbol is: " + symbol);
-
-            String userName = symbol + name;
-
-            Console.WriteLine("Your username is: " + userName);
+            Console.WriteLine(pi);
 
             Console.ReadKey();
         }
